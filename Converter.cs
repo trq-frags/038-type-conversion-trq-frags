70b17790652491d88de87d71b047d941a31b1230
track-frags <jadirks@anamosa.k12.ia.us>
2026-02-11 16:29:42 +0000

    weeweeweeweeweeweewee

--- Converter.cs
+++ Converter.cs
@@ -1,5 +1,19 @@
 ﻿namespace knightmoves;
 
 public class Converter{
-   //Add your code here
+      public static (int, int) ToTuple(string position){
+         char file = position[0];
+         char rank = position[1];
+   
+         int x = file - 'a';
+         int y = rank - '1';
+   
+         return (x, y);
+      }
+      public static string ToString((int, int) position){
+         char file = (char)('a' + position.Item1);
+         char rank = (char)('1' + position.Item2);
+   
+         return $"{file}{rank}";
+      }
 }
